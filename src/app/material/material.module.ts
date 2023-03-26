918c84d6e1ad189dd963a64bf1cbe89edbafbb12
Brian Pérez López <brianpl990227@gmail.com>
2023-03-26 18:08:21 -0400

    Cards de heroes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,7 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule]
+  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatCardModule]
 })
 export class MaterialModule { }
