533c7d69e9166a489baba5049a67434e5fb9b8b6
Brian Pérez López <brianpl990227@gmail.com>
2023-03-26 14:59:49 -0400

    Listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Los servicios al estar de manera global necesitan modulos de manera global
+//como el heroes.service necesita hacer peticions http se importa este modulo global aqui
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,6 +20,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../service/heroes.service';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  
+  heroes: Heroe[] = []
+
+  constructor(private heroesService: HeroesService){}
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe(response => {
+      this.heroes = response
+    })
+  }
+
 
 }
